392d2a9f3dcd7695f49cae79d32e30060b073454
Tobi <62397069+MagnusMight@users.noreply.github.com>
2024-04-02 12:11:48 -0400

    Update Scriptadmin.ps1
    coded script

--- Scriptadmin.ps1
+++ Scriptadmin.ps1
@@ -1,8 +1,36 @@
-#Created By Raptor
+﻿#REM==========================================
+#REM Create an Admin Password
+#REM==========================================
+#REM Created Date : 02 April 2024
+#REM Update Date  : 02 April  2024
+#REM Author : Raptor J (AKA Mecca)
+#REM 
+#REM Script Details:
+#REM --------------
+#REM  This script will:
+#REM       + Select the computer by name
+#REM       + then access the password file
+#REM       + Create a password for the account
+#REM 
+#REM===========================================
 
 
-#Settings for the auto logon process
-$PasswordSecured = Get-Content $PassFile | ConvertTo-SecureString -Key $Key
+
+
+$compname = $env:COMPUTERNAME
+$adminname = "DisabledAdmin"
+
+$file = "\\mxl0063knr\DeploymentShare11\Keys\Pass.txt"
+
+[Byte[]] $key = (1..16)
+
+#Settings for the password
+$PasswordSecured = Get-Content $File | ConvertTo-SecureString -Key $Key
 $marshal = [System.Runtime.InteropServices.Marshal]
 $bstr = $marshal::SecureStringToBSTR($PasswordSecured)
 $pass = $marshal::PtrToStringAuto($bstr) 
+
+
+
+#Set the password for the Disabled Admin account 
+([adsi]"WinNT://$computername/$adminname").setpassword($Pass)
