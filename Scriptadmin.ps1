5de3eb4e042f2f53fe86bb60a4ee2e7cb5728be9
Tobi <62397069+MagnusMight@users.noreply.github.com>
2024-04-02 11:35:58 -0400

    Update Scriptadmin.ps1
    pass command

--- Scriptadmin.ps1
+++ Scriptadmin.ps1
@@ -1,1 +1,8 @@
+#Created By Raptor
 
+
+#Settings for the auto logon process
+$PasswordSecured = Get-Content $PassFile | ConvertTo-SecureString -Key $Key
+$marshal = [System.Runtime.InteropServices.Marshal]
+$bstr = $marshal::SecureStringToBSTR($PasswordSecured)
+$pass = $marshal::PtrToStringAuto($bstr) 
