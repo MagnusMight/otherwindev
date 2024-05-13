5e2ee6a8ca69aa5dae8cf0edf7c27fef856eb056
Tobi <62397069+MagnusMight@users.noreply.github.com>
2024-05-13 15:47:36 -0500

    Update Check for update.ps1

--- Check for update.ps1
+++ Check for update.ps1
@@ -1,1 +1,12 @@
+$KBNumber = "KB5036892" # Replace XXXXXXX with the KB number of the update you want to check
+
+$Update = Get-HotFix | Where-Object {$_.HotFixId -eq $KBNumber}
+
+if ($Update) {
+    Write-Host "Update $KBNumber is installed."
+    exit 0
+} else {
+    Write-Host "Update $KBNumber is not installed."
+    exit 1
+}
 
